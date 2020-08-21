8d1f277baffa25c82ce81b7d85f5d146cf413fa0
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-08-21 14:37:22 -0400

    Generated three stickers, including one with an image background

--- R/BRMRH_sticker.R
+++ R/BRMRH_sticker.R
@@ -1,7 +1,7 @@
 library(hexSticker)
 library(jpeg)
 library(magick)
-imgurl <- "BRMRH.png"
+imgurl <- "Data/BRMRH.png"
 BRMRH = sticker(imgurl, package= "", p_size=10, h_fill = rgb(0.74, 0.2, 0.15, 1), p_y = 1, s_x=1, s_y=1, s_width=0.8, p_color = "white",
-            filename="Outputs/ISWGlogo1.png", h_color = rgb(0.74, 0.2, 0.15, 1))
+            filename="Outputs/BushfireHublogo.png", h_color = rgb(0.74, 0.2, 0.15, 1))
 plot(ISWG)
--- R/ISWG_sticker.R
+++ R/ISWG_sticker.R
@@ -1,7 +1,10 @@
 library(hexSticker)
 library(jpeg)
 library(magick)
-imgurl <- "ISWG3.png"
-ISWG = sticker(imgurl, package="ISWG", p_size=20, p_y = 1.65, s_x=1, s_y=0.85, s_width=0.7, p_color = "lightblue",
-            filename="Outputs/ISWGlogo1.png", h_color = "lightblue")
+library(ggplot2)
+library(ggimage)
+imgurl <- "Data/ISWG.png"
+ISWG = sticker(imgurl, package="ISWG", p_size=20, p_y = 1.65, s_x=1.03, s_y=0.85, s_width=0.75, p_color = "lightblue",
+            filename="Outputs/ISWGlogo1.png", h_color = "lightblue", spotlight = TRUE, l_y = 1.7) +
+  geom_hexagon(size = 1.2, fill = imgurl, color = "#87B13F")
 plot(ISWG)
